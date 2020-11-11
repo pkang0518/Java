2cd11e0a0251a292a593874f1966d3bcb22f0739
pkang0518 <philj.kang2@gmail.com>
2020-11-11 14:03:17 +0900

    no message

--- CastingEx02.java
+++
@@ -1,7 +0,0 @@
-public class CastingEx02 {
-    public static void main(String[] args) {
-        int i = 97;
-        System.out.println( i );
-        System.out.println( (char)i );
-    }
-}
--- OperatorEx02.java
+++
@@ -1,8 +0,0 @@
-public class OperatorEx02 {
-    public static void main(String[] args) {
-        // 정수 / 정수 => 정수 => 몫(소수점 이하 없어짐)
-        System.out.println( 3/ 2 ); // 1.5 그러나 출력값은 1
-        System.out.println( 3/ (double)2 ); // 소수점 출력을 위해 (double)
-        System.out.println( 3/ 2. ); // 소수점 출력을 위해 . 입력    
-    }
-}
